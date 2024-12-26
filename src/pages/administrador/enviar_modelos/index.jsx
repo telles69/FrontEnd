a2b384aec7d5b6029d5b3a4f859f373f41f4d528
telles69 <joao.telles@unochapeco.edu.br>
2024-12-26 17:36:25 -0300

    nao_aguento_mais

--- src/pages/administrador/enviar_modelos/index.jsx
+++ src/pages/administrador/enviar_modelos/index.jsx
@@ -3,7 +3,7 @@ import axios from '../../../utils/axios';
 import { useState, useEffect } from "react"
 import { FileUploadList, FileUploadRoot, FileUploadTrigger, FileUploadDropzone } from "@/components/ui/file-upload"
 import { Button, Card, Input, Stack, Flex, Center, Box, Image, Icon, Text, HStack, Link } from "@chakra-ui/react"
-import { Toaster ,toaster } from "@/components/ui/toaster"
+import { Toaster, toaster } from "@/components/ui/toaster"
 
 export default function Demo() {
 
@@ -14,8 +14,8 @@ export default function Demo() {
             toaster.create({
                 description: "Por favor, selecione um arquivo para upload.",
                 type: "error",
-              })
-          return;
+            })
+            return;
         }
         console.log("modelo")
         console.log(modelo)
@@ -25,76 +25,76 @@ export default function Demo() {
         console.log(formData)
         try {
             const response = await axios.post("http://localhost:3333/upload/modelo1", formData, {
-              headers: { "Content-Type": "multipart/form-data" },
+                headers: { "Content-Type": "multipart/form-data" },
             });
-      
+
             if (response.type === "success") {
                 toaster.create({
                     description: "Arquivo carregado com sucesso!",
                     type: "success",
-                  })
+                })
             } else {
                 toaster.create({
                     description: "Erro ao fazer upload do arquivo.",
                     type: "error",
-                  })
+                })
             }
-          } catch (error) {
+        } catch (error) {
             toaster.create({
                 description: "Erro ao enviar o arquivo.",
                 type: "error",
-              })
-          }
-        };
-      
-        const handleDrop = (e) => {
-          const file = e.target.files || e.dataTransfer.files;
-          if (file && file.length > 0) {
+            })
+        }
+    };
+
+    const handleDrop = (e) => {
+        const file = e.target.files || e.dataTransfer.files;
+        if (file && file.length > 0) {
             setModelo(file[0]);
-          }
+        }
     }
 
-    return(
+    return (
         <Box>
             <Background></Background>
             <Center minHeight="100vh">
-            <Box
-                width="25%"
-                height="25%"
-                p="6"
-                bg="rgba(255, 255, 255, 0.2)"
-                borderRadius="lg"
-                boxShadow="lg"
-                backdropFilter="blur(10px)"
-                border="1px solid rgba(255, 255, 255, 0.3)"
-            >
-                <Box textAlign="center" mb="6">
-                    <Image src="https://gainblers.com/imagenes/casas/pokerstarssports/pokerstars-logo.png" alt="LOGO" />
-                    <Box as="h2" fontSize="2xl" fontWeight="bold">
-                        Modelo documentação Usuario
+                <Box
+                    width="25%"
+                    height="25%"
+                    p="6"
+                    bg="rgba(255, 255, 255, 0.2)"
+                    borderRadius="lg"
+                    boxShadow="lg"
+                    backdropFilter="blur(10px)"
+                    border="1px solid rgba(255, 255, 255, 0.3)"
+                >
+                    <Box textAlign="center" mb="6">
+                        <Image src="https://gainblers.com/imagenes/casas/pokerstarssports/pokerstars-logo.png" alt="LOGO" />
+                        <Box as="h2" fontSize="2xl" fontWeight="bold">
+                            Modelo documentação Usuario
+                        </Box>
                     </Box>
+                    <Box>
+                        <FileUploadRoot maxW="xl" alignItems="stretch" maxFiles={10}>
+                            <FileUploadDropzone
+                                bg="rgba(255, 255, 255, 0.3)"
+                                label="Drag and drop here to upload"
+                                type="file"
+                                name="ola"
+                                description=".png, .jpg, .doc, .docx, .pdf up to 5MB"
+                                onDrop={handleDrop}
+                                onChange={handleDrop}
+                                accept=".png, .jpg, .doc, .docx, .pdf"
+                            />
+                            <FileUploadList modelo={modelo} showSize clearable />
+                        </FileUploadRoot>
+                    </Box>
+                    <Flex justifyContent="center" mt="3">
+                        <Button onClick={() => uploadFile()}>Enviar</Button>
+                    </Flex>
                 </Box>
-                <Box>
-                    <FileUploadRoot maxW="xl" alignItems="stretch" maxFiles={10}>
-                        <FileUploadDropzone
-                            bg="rgba(255, 255, 255, 0.3)"
-                            label="Drag and drop here to upload"
-                            type="file"
-                            name="ola"
-                            description=".png, .jpg, .doc, .docx, .pdf up to 5MB"
-                            onDrop={handleDrop}
-                            onChange = {handleDrop}
-                            accept=".png, .jpg, .doc, .docx, .pdf"
-                        />
-                        <FileUploadList modelo={modelo} showSize clearable/>
-                    </FileUploadRoot>
-                </Box>
-                <Flex justifyContent="center" mt="3">
-                    <Button onClick={() => uploadFile()}>Enviar</Button>
-                </Flex>
-            </Box>
-        </Center>
-        <Toaster />
+            </Center>
+            <Toaster />
         </Box>
     )
 }
